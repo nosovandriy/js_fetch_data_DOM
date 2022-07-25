5a7d9d790ecf182cb7e0f6d74e9236d7f635e2b2
Andriy Nosov <nosovandriy@gmail.com>
2022-07-25 14:30:58 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const body = document.querySelector('body');
+const ulList = document.createElement('ul');
+const url
+= 'https://mate-academy.github.io/phone-catalogue-static/api/phones.json';
+
+const request = () => {
+  return fetch(url)
+    .then(response => response.json())
+    .catch(error => {
+      setTimeout(() => error, 5000);
+    });
+};
+
+const getPhones = () => request();
+const getPhonesDetails = () => {
+  request()
+    .then(result => {
+      result.forEach(item => {
+        const itemLi = document.createElement('li');
+
+        itemLi.innerText = item.name;
+        ulList.append(itemLi);
+      });
+    });
+
+  body.append(ulList);
+};
+
+getPhones();
+getPhonesDetails();
